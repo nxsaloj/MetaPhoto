724b55b5175e6ed2c8d19d651491353ff83ee439
nxsaloj <niisaloj@gmail.com>
2024-08-05 19:58:59 -0600

    Updating the backend deployment script
    Setting up the bucket required for the lambda source code, passing parameters to the  file to properly execute locally the backend service, and deploy with the correct data as well

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 PROFILE="$1"
 ACCOUNT_ID="$2"
-REGION="${3:-us-east-1}"
+BUCKET_BUILD="${3:-metaphotobucketbuild}"
+REGION="${4:-us-east-1}"
 ROLE="arn:aws:iam::$ACCOUNT_ID:role/cloudformation-service-role"
 
-aws cloudformation validate-template --profile $PROFILE --region $REGION --template-body file://templates/role-template.yml
-aws cloudformation deploy --debug --profile $PROFILE --stack-name meta-photo-role-stack --region $REGION --template-file templates/role-template.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
-aws cloudformation validate-template --profile $PROFILE --region $REGION --template-body file://templates/main.yml
-aws cloudformation deploy --debug --profile $PROFILE  --region $REGION --role-arn $ROLE --stack-name meta-photo-stack --template-file templates/main.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+BASE_PARAMS="--profile $PROFILE --region $REGION"
+STAGE="${5:-prod}"
+
+aws cloudformation deploy --role-arn $ROLE \
+    --debug \
+    $BASE_PARAMS \
+    --stack-name meta-photo-stack \
+    --template-file MetaPhoto.spa/template.yml \
+    --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+    --parameter-overrides Stage=$STAGE BucketBuild=$BUCKET_BUILD
 aws s3 cp --profile $PROFILE --region $REGION index.html s3://meta-photo-bucket-test/ --acl public-read
